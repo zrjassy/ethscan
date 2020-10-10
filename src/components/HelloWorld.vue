946329dcff8e8a73944d0cb46e195b2247a6f8bf
zrjassy <834398599@qq.com>
2020-10-10 10:58:32 +0800

    update blocks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,6 +15,7 @@ export default {
   props: {
     msg: String
   }
+
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,119 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home" style="padding-top: 50px;">
+    <h4 style="text-align:center; margin-top: 30px;">
+      Latest Block: {{blockNumber}}
+    </h4>
+    <table class="pure-table" style="margin: auto" width='60%'>
+      <tbody>
+      <tr>
+        <th>Block #</th>
+        <th>Tx #</th>
+        <th>Size</th>
+        <th>Timestamp</th>
+      </tr>
+      <tr v-for="i in blocks" :key="i">
+        <td>{{i.number}}</td>
+        <td>{{i.transactions.length}}</td>
+        <td>{{i.size}}</td>
+        <td>{{i.timestamp}}</td>
+      </tr>
+      </tbody>
+    </table>
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// 局部注册组件
+// import HelloWorld from '@/components/HelloWorld.vue'
+import common from '@/common'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  data () {
+    return {
+      blocks: [],
+      maxBlocks: 0,
+      blockNumber: 0
+    }
+  },
+  created () {
+    const web3 = common.web3
+    this.maxBlocks = 50
+    web3.eth.getBlockNumber()
+      .then((result) => {
+        // console.log(result)
+        this.blockNumber = result
+        if (this.maxBlocks > result) {
+          this.maxBlocks = result + 1
+        }
+        for (let i = 0; i < this.maxBlocks; i++) {
+          // console.log(this.blockNumber)
+          web3.eth.getBlock(this.blockNumber - i)
+            .then((result) => {
+              console.log(result)
+              this.blocks.push(result)
+            })
+        }
+      })
   }
 }
 </script>
+
+<style type="text/css">
+html {
+  font-family: sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+}
+
+body {
+  margin: 10px;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+td,th {
+  padding: 0;
+}
+
+.pure-table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  empty-cells: show;
+  border: 1px solid #cbcbcb;
+}
+
+.pure-table caption {
+  color: #000;
+  font: italic 85%/1 arial,sans-serif;
+  padding: 1em 0;
+  text-align: center;
+}
+
+.pure-table td,.pure-table th {
+  border-left: 1px solid #cbcbcb;
+  border-width: 0 0 0 1px;
+  font-size: inherit;
+  margin: 0;
+  overflow: visible;
+  padding: .5em 1em;
+}
+
+.pure-table thead {
+  background-color: #e0e0e0;
+  color: #000;
+  text-align: left;
+  vertical-align: bottom;
+}
+
+.pure-table td {
+  background-color: transparent;
+}
+</style>
